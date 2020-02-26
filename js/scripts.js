223c9a0b4bb4bbacc2909f205540c29b82ec79bb
luiguzman <59583114+luiguzman@users.noreply.github.com>
2020-02-26 11:45:32 -0800

    JavaScript on svgs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('OK');
+gsap.to('#cookie',{
+    rotation:1080,
+    duration:80
+});
+gsap.to('#donut',{
+    rotation:1080,
+    duration:80
+});
+gsap.fromTo('#loli',{x:0,y:0},{
+    x:0,
+    y:10,
+    repeat:-1,
+    yoyo:true,
+
+});
